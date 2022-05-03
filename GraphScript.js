8b4e8569299e7da23df166c04321e869d33171e4
Ericpso <ericpso.89@gmail.com>
2022-05-03 17:06:54 -0300

    Hardcoded Sales data in JavaScript

--- GraphScript.js
+++ GraphScript.js
@@ -1,4 +1,24 @@
-var Categoria = ["Roupa", "Joias", "Cosméticos"];
-var Produtos = ["Camisa", "Calça", "Perfume"];
-var Marcas = ["Camisa1", "Camisa2", "Calça1", "Calça2", "Perfume1", "Perfume2", "Perfume3"];
+// Initializing Product List:
+// This list should be loaded from a file or database
+const roupas = ["Camisas", "Calças"]
+const joias = ["Brincos","Cordões"]
+const cosméticos = ["Perfume"]
 
+const camisas = ["Camisa1","Camisa2"]
+const calças = ["Calça1", "Calça2", "Calça3"]
+const brincos = ["Brinco1","Brinco2"]
+const cordões = ["Cordão1","Cordão2"]
+const perfumes = ["Perfume1", "Perfume2"]
+
+// Sales data by month
+const Camisa1 = [450, 500, 400, 450]
+const Camisa2 = [400, 200, 100, 500]
+const Calça1 = [100, 250, 300, 450]
+const Calça2 = [100, 400, 100, 234]
+const Calça3 = [621, 250, 567, 400]
+const Brinco1 = [100, 100, 300, 100]
+const Brinco2 = [210, 200, 200, 500]
+const Cordão1 = [100, 400, 150, 450]
+const Cordão2 = [200, 200, 650, 400]
+const Perfume1 = [100, 400, 500, 100]
+const Perfume2 = [550, 200, 150, 300]
